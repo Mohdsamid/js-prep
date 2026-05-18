88b5e9b52c28fc9dda773ef34883a94cbaa52607
Mohd Samid <mohdsamid78@gmail.com>
2026-05-19 01:25:15 +0530

    Polishing style and clean code

--- login/basic.js
+++ login/basic.js
@@ -1,46 +1,63 @@
-let formdata = document.querySelector("#formdata")
-formdata.addEventListener("submit",(e)=>{
-    e.preventDefault()
+// ================= SIGN UP =================
 
-    let data = {
-    name:formdata[0].value,
-    email:formdata[1].value,
-    pass:formdata[2].value,
+let formdata = document.querySelector("#formdata");
+
+if(formdata){
+
+    formdata.addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+        let data = {
+            name: formdata[0].value,
+            email: formdata[1].value,
+            pass: formdata[2].value,
+        };
+
+        localStorage.setItem("userdata", JSON.stringify(data));
+
+        alert("Signup Successfully");
+
+        // formdata.reset();
+    });
 
 }
-// console.log(data)
-localStorage.setItem("userdata",JSON.stringify(data));
-})
-    console.log(formdata[0].value)
-    console.log(formdata[1].value)
-    console.log(formdata[2].value)
 
-// let 
+// ================= LOGIN =================
 
-let logdata = document.querySelector("#logindata")
-logdata.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    let login = {
-        email:logdata[0].value,
-        pass:logdata[1].value
-    }
+let logdata = document.querySelector("#logindata");
 
-    let lsd= localStorage.getItem("userdata")
-    console.log(lsd)
-    let formdata = JSON.parse(lsd)
-    console.log(formdata)
-    if(login.email===formdata.email && login.pass===formdata.pass){
-        alert("Log in successfully")
-    }
-    else{
-        alert("Errorrrr")
-    }
-})
-// console.log(data);
-// console.log(logdata);
-// let name = document.getElementById("name
+if(logdata){
 
-// localStorage.setItem("name",formdata[0].value)
-// localStorage.getItem(formdata[0].value)
-// localStorage.getItem(formdata[1].value)
-// localStorage.getItem(formdata[2].value)
+    logdata.addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+        let login = {
+            email: logdata[0].value,
+            pass: logdata[1].value
+        };
+
+        let lsd = localStorage.getItem("userdata");
+
+        if(lsd){
+
+            let userdata = JSON.parse(lsd);
+
+            if(
+                login.email === userdata.email &&
+                login.pass === userdata.pass
+            ){
+                alert("Login Successfully");
+            }
+            else{
+                alert("Invalid Email or Password");
+            }
+
+        }
+        else{
+            alert("No User Found");
+        }
+
+        logdata.reset();
+    });
+
+}
